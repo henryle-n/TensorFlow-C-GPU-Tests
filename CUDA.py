87b9b66eac4997d91c32be028361507b2c52bcf5
Henry Le <henryle.b11@gmail.com>
2020-06-30 18:45:32 -0500

    edit cuda.py

--- CUDA.py
+++ CUDA.py
@@ -8,15 +8,23 @@ cdr.init()
 
 print ('Author : Henry Le')
 print (("=")*40)
-print ('Detected {} CUDA Capable device(s)'.format(cdr.Device.count()))
+
+if cdr.Device.count() > 1:
+    dev = 'devices'
+else:
+    dev = 'device'
+    
+print (f'Detected :: ({cdr.Device.count()}) CUDA capable {dev}')
 
 for i in range(cdr.Device.count()):
     
-    gpu_device = cdr.Device(i)
-    print ('Device {}: {}'.format( i, gpu_device.name()))
-    compute_capability = float( '%d.%d' % gpu_device.compute_capability() )
-    print ('\t Compute Capability: {}'.format(compute_capability))
-    print ('\t Total Memory: {} megabytes'.format(gpu_device.total_memory()//(1024**2)))
+    gpu_device = cdr.Device(i)  
+    print (f'Device [{i}] :: {gpu_device.name()}')
+    
+    compute_capability = float('%d.%d' % gpu_device.compute_capability())
+    print (f'\t Compute Capability: {compute_capability}')
+    # in binary, 1 MB = 2^20 Bytes, in SI, simply 1 MB = 10^6 Bytes
+    print (f'\t Total Memory: {gpu_device.total_memory()//(2**20)} GB')
     
   
     device_attr_tuples = gpu_device.get_attributes().items() 
@@ -24,29 +32,66 @@ for i in range(cdr.Device.count()):
     
     for k, v in device_attr_tuples:
         device_attr[str(k)] = v
-    print("Device Attributes : ", "\n", ("-")*30, "\n", device_attr)
     
     num_mp = device_attr['MULTIPROCESSOR_COUNT']
     
     # http://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#compute-capabilities
-    # extract the cuda core per processor based on retrieved compute_capability
-    cuda_cores_per_mp = { 
-        5.0 : 128,
-        5.2 : 128,
-        5.3 : 16,
-        6.0 : 128,
-        6.1 : 32,
-        6.2 : 16,
-        7.0 : 128,
-        7.2 : 16,
-        7.5 : 128,
-        8.0 : 128}[compute_capability]
-  
-
-    print ('\t ({}) Multiprocessors, ({}) CUDA Cores \n\t Multiprocessor: Total ({}) CUDA Cores'.format(num_mp, cuda_cores_per_mp, num_mp*cuda_cores_per_mp))
+    # Devices with the same major revision number are of the same core architecture. 
+    # The major revision number is:
+        # 8 for devices based on the NVIDIA Ampere GPU architecture
+        # 7 for devices based on the Volta architecture
+        # 6 for devices based on the Pascal architecture
+        # 5 for devices based on the Maxwell architecture
+        # 3 for devices based on the Kepler architecture
+        # 2 for devices based on the Fermi architecture
+        # 1 for devices based on the Tesla architecture.
     
+    
+    # extract the cuda core per processor based on retrieved compute_capability
+    def cuda_per_sm(cap_maj, cap_min):
+        return {
+            # Tesla
+            (1, 0):   8,      # SM 1.0
+            (1, 1):   8,      # SM 1.1
+            (1, 2):   8,      # SM 1.2
+            (1, 3):   8,      # SM 1.3
+            
+            # Fermi
+            (2, 0):  32,      # SM 2.0: GF100 class
+            (2, 1):  48,      # SM 2.1: GF10x class
+            
+            # Kepler
+            (3, 0): 192,      # SM 3.0: GK10x class
+            (3, 2): 192,      # SM 3.2: GK10x class
+            (3, 5): 192,      # SM 3.5: GK11x class
+            (3, 7): 192,      # SM 3.7: GK21x class
+            
+            # Maxwell
+            (5, 0): 128,      # SM 5.0: GM10x class
+            (5, 2): 128,      # SM 5.2: GM20x class
+            (5, 3): 128,      # SM 5.3: GM20x class
+            
+            # Pascal
+            (6, 0):  64,      # SM 6.0: GP100 class
+            (6, 1): 128,      # SM 6.1: GP10x class
+            (6, 2): 128,      # SM 6.2: GP10x class
+            
+            # Volta
+            (7, 0):  64,      # SM 7.0: GV100 class
+            (7, 2):  64,      # SM 7.2: GV11b class
+            
+            # Turing
+            (7, 5):  64,      # SM 7.5: TU10x class
+            
+        }.get((cap_maj, cap_min), 0)   # unknown architecture : return ) to flag for user
+
+    cuda_cores_per_sm = cuda_per_sm(device_attr['COMPUTE_CAPABILITY_MAJOR'], device_attr['COMPUTE_CAPABILITY_MINOR'])
+  
+    print (f'\t Streaming Processors: {num_mp}\n\t CUDA Cores per Streaming Processor: {cuda_cores_per_sm}\n\t Total CUDA Cores: {num_mp*cuda_cores_per_sm}')
+    
+    # remove MULTIPROCESSOR_COUNT to print the rest of attributes, no need for repeat
     device_attr.pop('MULTIPROCESSOR_COUNT')
     
     for k, v in device_attr.items():
-        print ('\t {}: {}'.format(k, v))
+        print (f'\t {k}: {v}')
         
--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # TensorFlow-C/GPU-Tests
 ## Background
-Out of my curiosity in the hardware world, I want to validate the statement that many ML / AI Engineers made all over the internet: Traning models on GPU is 5-10+ faster than on CPU. I set up a simple Jupyter Notebook with two kernels (run on two different venv, one utilizes only cpu, and the other utilizes GPU). Results of TensorFlow training time by utilizing CPU vs. GPU then printed out for comparison.
-
-In my case set-up, the CPU took ~13.4 mins to complete while the GPU get the job done in ~1.6 mins. That's **8.4** times faster.
+Out of my curiosity for hardware speed in the world of Machine Learning (ML), I want to validate the statement that many ML / AI Engineers made all over the internet: Traning models on GPU is 5-10+ (pending on the model) faster than on CPU. I set up a simple Jupyter Notebook with two kernels (run on two different venv, one utilizes only cpu, and the other utilizes only GPU). TensorFlow training time by utilizing CPU vs. GPU is then clocked and printed out for comparison.
 
 ## Benefit of ML with GPU
-CPU is the brain of our computing device, either a high-end server, a desktop, or a laptop. Utilizing GPU for ML brought benefits including: faster model training, and freeing up CPU resources for other tasks/ processes that GPU can't be utilized. 
+CPU is the brain of our computing devices, no matter if it is a high-end server, a desktop, or a laptop, or just a simple tablet. Utilizing GPU for ML brought benefits including: 
+* Faster model training
+* Freeing up CPU resources for other tasks/ processes that GPU can't be utilized
+
+## Differences between GPU and CPU:
+* CPU has few complicated cores (4x2 in this set up) which run processes sequentially, with a few threads at a time 
+* GPU has more cores which allow parallel computing with thousands of threads at a time.
+* In deep learning, the host code runs on CPU where as CUDA code runs on GPU.
+* GPU is bandwidth optimized (carry multiple large size packages, in trade-off for speed)
+* CPU is latency (i.e. memory/RAM access time, high speed but fewer and smaller packages) optimized
+* Bandwidth of GPU is significantly larger than that of CPU, thanks to the VRAM
+* Thread parallelism in GPU surpasses the latency, 
+
 ## Data Set-up:
 * Utilizing MNIST Dataset: http://yann.lecun.com/exdb/mnist/
     * 60,000 digit images
 * Keras instruction: https://keras.io/api/datasets/mnist/
-* 30 Epochs was run
-
+* 30 Epochs were run for comparison
 
 ## Hardware:
 * Intel(R) Core(TM) i7-7700K CPU @ 4.20GHz
@@ -26,7 +35,6 @@ CPU is the brain of our computing device, either a high-end server, a desktop, o
 	* L2 cache:	1.0 MB
 	* L3 cache:	8.0 MB
     * Full Specs: <a href="https://ark.intel.com/content/www/us/en/dark/products/97129/intel-core-i7-7700k-processor-8m-cache-up-to-4-50-ghz.html">Intel Website</a>
-
 
 * MSI NVIDIA GeForce RTX 2070 SUPER GAMING X
     * BIOS Version: 90.04.86.00.62
